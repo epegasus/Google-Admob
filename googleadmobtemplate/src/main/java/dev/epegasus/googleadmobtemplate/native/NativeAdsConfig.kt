10906d0940449f50a6a937005a7153be2d8de438
Sohaib <sohaibahmed.hypersoft@gmail.com>
2022-09-28 12:51:16 +0500

    Added Normal Native Height

--- googleadmobtemplate/src/main/java/dev/epegasus/googleadmobtemplate/native/NativeAdsConfig.kt
+++ googleadmobtemplate/src/main/java/dev/epegasus/googleadmobtemplate/native/NativeAdsConfig.kt
@@ -13,6 +13,7 @@ import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.android.gms.ads.nativead.NativeAd
 import dev.epegasus.googleadmobtemplate.LogUtils.showAdsLog
 import dev.epegasus.googleadmobtemplate.databinding.AdmobNativeMediumBinding
+import dev.epegasus.googleadmobtemplate.databinding.AdmobNativeNormalBinding
 import dev.epegasus.googleadmobtemplate.native.interfaces.OnNativeAdLoad
 import dev.epegasus.googleadmobtemplate.native.interfaces.OnNativeResponseListener
 
@@ -100,4 +101,88 @@ class NativeAdsConfig(private val context: Context) {
         clAdContainer.removeAllViews()
         clAdContainer.addView(binding.root)
     }
+
+    fun checkNativeAdNormal(
+        nativeAdID: String, isInternetConnected: Boolean, isRemoteConfig: Int, isBillingRequired: Boolean,
+        cl_ad_container: FrameLayout, ll_loading: LinearLayout,
+        onResponseListener: OnNativeResponseListener,
+        onNativeAdLoad: OnNativeAdLoad,
+    ) {
+        val binding = AdmobNativeNormalBinding.inflate(LayoutInflater.from(context))
+        if (isInternetConnected && (isRemoteConfig == 1) && isBillingRequired) {
+            val adLoader: AdLoader = AdLoader.Builder(context, nativeAdID)
+                .forNativeAd { nativeAd ->
+                    if ((context as Activity).isDestroyed) {
+                        showAdsLog(context, "checkNativeAdNormal", "isDestroyed", "Destroying Native, fragment not found")
+                        nativeAd.destroy()
+                        return@forNativeAd
+                    }
+                    populateNormalLayout(cl_ad_container, binding, nativeAd)
+                    onNativeAdLoad.onNativeAdLoad(nativeAd)
+                }
+                .withAdListener(object : AdListener() {
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        super.onAdFailedToLoad(loadAdError)
+                        showAdsLog(context, "checkNativeAdNormal", "onAdFailedToLoad", loadAdError.message)
+                        cl_ad_container.removeAllViews()
+                        cl_ad_container.visibility = View.GONE
+                        onResponseListener.onResponse()
+                    }
+
+                    override fun onAdLoaded() {
+                        super.onAdLoaded()
+                        showAdsLog(context, "checkNativeAdNormal", "onAdLoaded", "loaded")
+                        ll_loading.visibility = View.GONE
+                        onResponseListener.onResponse()
+                    }
+                })
+                .build()
+            adLoader.loadAd(AdManagerAdRequest.Builder().build())
+        } else {
+            showAdsLog(context, "checkNativeAdNormal", "else", "called")
+            cl_ad_container.removeAllViews()
+            cl_ad_container.visibility = View.GONE
+            onResponseListener.onResponse()
+        }
+    }
+
+    private fun populateNormalLayout(clAdContainer: FrameLayout, binding: AdmobNativeNormalBinding, nativeAd: NativeAd) {
+        binding.apply {
+            root.apply {
+                // refer all our views to NativeAdView
+                mediaView = mvMedia
+                headlineView = tvTitle
+                bodyView = tvBody
+                iconView = ifvAdIcon
+                advertiserView = tvAd
+                callToActionView = btnAction
+
+                setNativeAd(nativeAd)
+            }
+
+            // Title & Body
+            tvTitle.text = nativeAd.headline
+            tvBody.text = nativeAd.body
+
+            // Ad
+            nativeAd.advertiser?.let { tvAd.text = it }
+
+            // Icon
+            nativeAd.icon?.let {
+                ifvAdIcon.setImageDrawable(it.drawable)
+            } ?: kotlin.run {
+                ifvAdIcon.visibility = View.GONE
+            }
+
+            // Button
+            nativeAd.callToAction?.let {
+                btnAction.text = it
+            } ?: kotlin.run {
+                btnAction.visibility = View.GONE
+            }
+        }
+        clAdContainer.removeAllViews()
+        clAdContainer.addView(binding.root)
+    }
+
 }
